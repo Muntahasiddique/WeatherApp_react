0823d71114a3eebb7ffe0a557a7d6492daf55fd3
muntaha <muntahamirza890@gmail.com>
2025-09-07 03:49:55 +0500

    Added intial files for weather App

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import '../styles/components/Main.scss'
+
+const Main = () => {
+  return (
+    <div className='Main' >
+      Main
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Place.jsx
@@ -0,0 +1,11 @@
+
+
+function Place() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Place
