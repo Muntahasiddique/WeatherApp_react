3f935882d154f96634186630ad4397bdf8c9493a
muntaha <muntahamirza890@gmail.com>
2025-09-07 05:10:57 +0500

    Added the api distructuring

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -3,6 +3,10 @@ import {getCurrentWeather} from '../api/'
 function CurrentWeather() {
     const data = getCurrentWeather()
     console.log(data)
+//destructuring
+const {cloud_cover,feels_like,humidity,icon_num, precipitation, summary , temperature ,uv_index , visibility,wind
+} = data
+
   return (
     <div className="CurrentWeather" >
       
